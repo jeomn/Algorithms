150ece72fcf48df27e4e2654d947657f8c55f9ab
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-05 10:43:57 +0900

    Update 핸드폰 번호 가리기.py

--- programmers/Level1/핸드폰 번호 가리기.py
+++ programmers/Level1/핸드폰 번호 가리기.py
@@ -4,3 +4,12 @@ def solution(phone_number):
         answer += "*"
     answer += phone_number[len(phone_number)-4:]
     return answer
+
+
+#20210105_review
+"""
+def solution(phone_number):
+    phone_number = list(phone_number)
+    phone_number[:-4] = "*"*len(phone_number[:-4])
+    return ''.join(phone_number)
+"""
