bddb6d162338c933757a5433ec4714b60b66f03c
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-05 10:34:45 +0900

    Update 하샤드 수.py

--- programmers/Level1/하샤드 수.py
+++ programmers/Level1/하샤드 수.py
@@ -5,3 +5,26 @@ def solution(x):
     else:
         answer = False
     return answer
+
+
+#20210105_review
+"""
+def solution(x):
+    x_list = []
+    cx = x
+    for i in range(x):
+        if x >= 10:
+            x_list.append(x%10)
+            x //= 10
+        else:
+            x_list.append(x%10)
+            break
+    print(x_list)
+    sum_x = sum(x_list)
+    print(sum_x)
+    if cx%sum_x == 0:
+        answer = True
+    else:
+        answer = False
+    return answer
+"""
