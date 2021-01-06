4eb5de5ed0f7d3a4d167b47599a7a884d43cae04
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-06 11:40:07 +0900

    Update 서울에서 김서방 찾기.py

--- programmers/Level1/서울에서 김서방 찾기.py
+++ programmers/Level1/서울에서 김서방 찾기.py
@@ -2,3 +2,9 @@ def solution(seoul):
     for i in range(0, len(seoul)):
         if seoul[i] == 'Kim':
             return "김서방은 " + str(i) + "에 있다"
+
+        
+#20210106_review
+def solution(seoul):        
+    place = seoul.index("Kim")
+    return "김서방은 " + str(place) + "에 있다"
