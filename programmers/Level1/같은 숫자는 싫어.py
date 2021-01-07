7c3c3354c0e92fe0e0b26e23c7ae6c763a17231f
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-07 11:00:21 +0900

    Update 같은 숫자는 싫어.py

--- programmers/Level1/같은 숫자는 싫어.py
+++ programmers/Level1/같은 숫자는 싫어.py
@@ -5,3 +5,12 @@ def solution(arr):
         if arr[i-1] != arr[i]:
             answer.append(arr[i])
     return answer
+
+
+#20210107_review
+def solution(arr):
+    answer = [arr[0]]
+    for i in range(1, len(arr)):
+        if arr[i-1]  != arr[i]:
+            answer.append(arr[i])         
+    return answer
