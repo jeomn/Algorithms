5356e65244184848f20c00183528a1ec7d25edda
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-07 09:41:13 +0900

    Update 체육복.py

--- programmers/Level1/체육복.py
+++ programmers/Level1/체육복.py
@@ -1,3 +1,5 @@
+#탐욕법
+
 def solution(n, lost, reserve):
     set_lost = set(lost) - set(reserve)
     set_reserve = set(reserve) - set(lost)
@@ -9,3 +11,18 @@ def solution(n, lost, reserve):
             set_lost.remove(l+1)
     
     return n - len(set_lost)
+
+
+#20210107_review
+def solution(n, lost, reserve):
+    set_lost = set(lost) - set(reserve)
+    set_reserve = set(reserve) - set(lost)
+    
+    for i in set_reserve:
+        if i-1 in set_lost:
+            set_lost.remove(i-1)
+        elif i+1 in set_lost:
+            set_lost.remove(i+1)
+    
+    answer = n - len(set_lost)
+    return answer
