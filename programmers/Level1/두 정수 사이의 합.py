9ebbe0fd36913a4471b53f0280d88c130065f074
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-07 11:08:51 +0900

    Update 두 정수 사이의 합.py

--- programmers/Level1/두 정수 사이의 합.py
+++ programmers/Level1/두 정수 사이의 합.py
@@ -5,3 +5,13 @@ def solution(a, b):
     for i in range(a, b+1):
         answer += i
     return answer
+
+
+#20210107_review
+def solution(a, b):
+    answer = 0
+    n = abs(a-b)
+    m = min(a, b)
+    for i in range(m, m+n+1):
+        answer += i
+    return answer
