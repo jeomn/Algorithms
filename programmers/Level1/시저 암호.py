7c408e2f67dbb94a9458520ea09ba60f84aa1b32
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-07 11:27:34 +0900

    Update 시저 암호.py

--- programmers/Level1/시저 암호.py
+++ programmers/Level1/시저 암호.py
@@ -6,3 +6,16 @@ def solution(s, n):
         elif s[i].islower():
             s[i] = chr((ord(s[i]) - ord('a') + n) % 26 + ord('a'))
     return "".join(s)
+
+
+#20210107_review
+def solution(s, n):
+    answer = ''
+    for i in range(0, len(s)):
+        if s[i].isupper():
+            answer += chr((ord(s[i]) - ord('A') + n)%26 + ord('A'))
+        elif s[i].islower():
+            answer += chr((ord(s[i]) - ord('a') + n)%26 + ord('a'))
+        else:
+            answer += s[i]
+    return answer
