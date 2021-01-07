2b0fd596767f641cdf86f6b0e3e6f100073d720e
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-07 10:48:21 +0900

    Update 가운데 글자 가져오기.py

--- programmers/Level1/가운데 글자 가져오기.py
+++ programmers/Level1/가운데 글자 가져오기.py
@@ -5,3 +5,12 @@ def solution(s):
     else:
         answer = s[(len(s)//2)]
     return answer
+
+
+#20210107_review
+def solution(s):
+    num_s = len(s)
+    if num_s%2 == 0:
+        return s[(num_s//2)-1 : (num_s//2)+1]
+    elif num_s%2 ==1:
+        return s[num_s//2]
