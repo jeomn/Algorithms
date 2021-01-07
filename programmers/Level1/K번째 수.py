2de04641f6480e15ed2170a5d06f7e94f12e791e
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-07 09:58:56 +0900

    (정렬) K번째 수

--- programmers/Level1/K번째 수.py
+++ programmers/Level1/K번째 수.py
@@ -1,3 +1,4 @@
+#정렬
 def solution(array, commands):
     answer = []
     for n in range(0, len(commands)):
@@ -6,4 +7,18 @@ def solution(array, commands):
         k = commands[n][2]
         array_part = sorted(array[(i-1):j])
         answer.append(array_part[k-1])
+    return answer
+
+
+#20210107_review
+def solution(array, commands):
+    answer, _array = [], []
+    for i in range(0, len(commands)):
+        f = commands[i][0]-1
+        l = commands[i][1]
+        n = commands[i][2]-1
+        _array = array[f:l]
+        _array.sort()
+        answer.append(_array[n])
+    
     return answer
