5e1ea9383bc14611a96c3bf7894b035ecc0c4a5e
jeomn <46081831+jeomn@users.noreply.github.com>
2021-01-08 10:54:41 +0900

    Update 완주하지 못한 선수.py

--- programmers/Level1/완주하지 못한 선수.py
+++ programmers/Level1/완주하지 못한 선수.py
@@ -4,3 +4,12 @@ def solution(participant, completion):
     answer = collections.Counter(participant) - collections.Counter(completion)
     
     return ''.join(answer)
+
+
+#20210108_review
+from collections import Counter
+
+def solution(participant, completion):
+    n_com = Counter(participant) - Counter(completion)
+    answer = list(n_com)[0]
+    return answer
