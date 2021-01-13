f9a8e69a67c46bbf7a5022492dfa24537d6467e6
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-01-13 10:52:55 +0900

    Create 올바른 괄호.py

---
+++ programmers/Level2/올바른 괄호.py
@@ -0,0 +1,25 @@
+from collections import Counter
+
+def solution(s):
+    answer = True
+    i = 0
+    j = len(s)-1
+    s_stack = []
+    
+    count_s = Counter(s)
+    if count_s['('] != count_s[')']:
+        return False
+    else:
+        for i in s:
+            if i == '(':
+                s_stack.append(i)
+            #'('가 아니면 ==> ')'이면
+            else:
+                if len(s_stack) >= 1:
+                    s_stack.pop()
+                #stack에 값이 없는데 ')'이면
+                else:
+                    return False
+    if len(s_stack) != 0:
+        answer = False
+    return answer
