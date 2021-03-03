47fc5bf3b8b91b0807d5dfdf00ea07fc8f98667b
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-03-03 21:09:00 +0900

    Create 게임 맵 최단거리.py

---
+++ programmers/Level2/게임 맵 최단거리.py
@@ -0,0 +1,27 @@
+#찾아라 프로그래밍 마에
+from collections import deque
+
+def solution(maps):
+    start = [0, 0]
+    n, m = len(maps[0]), len(maps)
+    visited = [[0]*n for _ in range(m)]
+    visited[0][0] = 1
+    route = deque([start])
+    
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
+    while route:
+        x, y = route.popleft()
+        for i in range(4):
+            nx, ny = dx[i] + x, dy[i] + y
+            
+            if 0 <= nx < m and 0 <= ny <n:
+                if visited[nx][ny] == 0 and maps[nx][ny] == 1:
+                    visited[nx][ny] = visited[x][y] + 1
+                    route.append([nx, ny])
+    
+    answer = visited[m-1][-1]
+    if answer == 0:
+        return -1
+    else:
+        return answer
