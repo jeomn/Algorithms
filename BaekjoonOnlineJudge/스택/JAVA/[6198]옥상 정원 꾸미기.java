08fd3344b8aa82837d648c0f2c862056c2d05fb8
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-08-06 03:57:07 +0900

    Update [6198]옥상 정원 꾸미기.java

--- BaekjoonOnlineJudge/스택/JAVA/[6198]옥상 정원 꾸미기.java
+++ BaekjoonOnlineJudge/스택/JAVA/[6198]옥상 정원 꾸미기.java
@@ -1,3 +1,14 @@
+/*
+자료형... long은 int보다 큰 정수, double은 실수
+
+ArrayList<Integer> input = new ArrayList<>();
+  //데이터 처리
+Stack<Integer> buildings = new Stack<>();
+Collections.reverse(input);     //List 뒤집기
+buildings.addAll(input);        //List 통으로 stack 넣기
+*/
+
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Stack;
