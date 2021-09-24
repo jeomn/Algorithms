d31e4730e5cdb0eb7cf13fc9da17b8d115a2dd7f
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-09-24 11:05:43 +0900

    Update [7576]토마토.java

--- JAVA_BaekjoonOnlineJudge/DFS와 BFS/[7576]토마토.java
+++ JAVA_BaekjoonOnlineJudge/DFS와 BFS/[7576]토마토.java
@@ -81,3 +81,78 @@ public class Main {
 		}
 	}
 }
+
+
+
+/*
+*	20210924
+*	REVIEW 1
+*/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_7576_Tomatoes {
+	
+	static int N, M;
+	static int[][] box;
+	static int[] dx = {-1, 1, 0, 0};
+	static int[] dy = {0, 0, -1, 1};
+
+	public static void main(String[] args)throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		M = Integer.parseInt(st.nextToken());
+		N = Integer.parseInt(st.nextToken());
+		
+		box = new int[N][M];
+		LinkedList<int[]> tomato = new LinkedList<>();
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<M; c++) {
+				box[r][c] = Integer.parseInt(st.nextToken());
+				if(box[r][c] == 1) tomato.add(new int[] {r, c});
+			}
+		}
+		
+		int day = ripenTomatoes(tomato);
+		check: for(int r=0; r<N; r++) {
+			for(int c=0; c<M; c++) {
+				if(box[r][c] == 0) {
+					day = -1;
+					break check;
+				}
+			}
+		}
+		System.out.println(day);
+	}
+	
+	
+	private static int ripenTomatoes(LinkedList<int[]> start) {
+		Queue<int[]> route = start;
+		
+		int day = 0;
+		while(!route.isEmpty()) {
+			int[] coord = route.poll();
+			int r = coord[0];
+			int c = coord[1];
+			
+			for(int i=0; i<4; i++) {
+				int nr = r+dx[i];
+				int nc = c+dy[i];
+				
+				if(nr<0 || nr>=N || nc<0 || nc>=M) continue;
+				if(box[nr][nc] != 0) continue;
+				
+				route.add(new int[] {nr, nc});
+				box[nr][nc] = box[r][c]+1;
+				day = Math.max(day, box[nr][nc]-1);
+			}
+		}		
+		
+		return day;
+	}
+}
