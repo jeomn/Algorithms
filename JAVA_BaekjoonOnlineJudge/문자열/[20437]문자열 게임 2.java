b9fc423596faafded6dbcda1c52295335231589e
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-11-01 03:14:33 +0900

    Create [20437]문자열 게임 2.java

---
+++ JAVA_BaekjoonOnlineJudge/문자열/[20437]문자열 게임 2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class Main {
+	
+	static String W;
+	static int K, Wlength, minLength, maxLength;
+	static int[] alphabet;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int T = Integer.parseInt(br.readLine());
+		for(int t=0; t<T; t++) {
+			W = br.readLine();
+			K = Integer.parseInt(br.readLine());
+			
+			Wlength = W.length();
+			alphabet = new int[26];
+			for(int i=0; i<Wlength; i++) {
+				alphabet[W.charAt(i)-'a']++;
+			}
+			
+			minLength = Integer.MAX_VALUE;
+			maxLength = -1;
+			findWord();
+			if(minLength==Integer.MAX_VALUE) System.out.println("-1");
+			else System.out.println(minLength+" "+maxLength);
+		}
+	}
+
+	private static void findWord() {
+		for(int i=0; i<Wlength; i++) {
+			char c = W.charAt(i);
+			if(alphabet[c-'a'] < K) continue;
+			
+			int cnt=0, j=0;
+			for(j=i; j<Wlength; j++) { //cnt=1 / j=i+1로 하면 5%에서 계속 틀렸음
+				if(W.charAt(j)==c) cnt++;
+				if(cnt==K) {
+					int length = j-i+1;
+					minLength = Math.min(minLength, length);
+					maxLength = Math.max(maxLength, length);
+					break;
+				}
+			}
+		}
+	}
+}
