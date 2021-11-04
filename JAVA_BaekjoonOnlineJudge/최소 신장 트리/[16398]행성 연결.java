23d4c7e0371b7d1876f13ed37454f1ac91c8d403
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-11-05 00:02:14 +0900

    Create [16398]행성 연결.java

---
+++ JAVA_BaekjoonOnlineJudge/최소 신장 트리/[16398]행성 연결.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	static int N;
+	static long minWeight;
+	static int[] parents;
+	static PriorityQueue<Node> planets;
+	static class Node implements Comparable<Node>{
+		int s, e, w;
+		Node(int s, int e, int w){
+			this.s = s;
+			this.e = e;
+			this.w = w;
+		}
+		
+		public int compareTo(Node o) {
+			return this.w - o.w;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;// = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(br.readLine());
+		
+		planets = new PriorityQueue<>();
+		for(int r=0; r<N; r++) {
+			st = new StringTokenizer(br.readLine());
+			for(int c=0; c<N; c++) {
+				int w = Integer.parseInt(st.nextToken());
+				if(w==0) continue;
+				planets.add(new Node(r, c, w));
+			}
+		}
+		
+		parents = new int[N];
+		for(int i=0; i<N; i++) {
+			parents[i] = i;
+		}
+		
+		kruskal();
+		
+		System.out.println(minWeight);
+	}
+	
+	public static int findParent(int n) {
+		if(parents[n] == n) return n;
+		else return parents[n] = findParent(parents[n]);
+	}
+	
+	public static void union(int a, int b) {
+		int rootA = findParent(a);
+		int rootB = findParent(b);
+		
+		if(rootA == rootB) return;
+		if(rootA < rootB) parents[rootB] = rootA;
+		else parents[rootA] = rootB;
+	}
+	
+	public static void kruskal() {
+		while(!planets.isEmpty()) {
+			Node p = planets.poll();
+			if(findParent(p.s) == findParent(p.e)) continue;
+			minWeight += p.w;
+			union(p.s, p.e);
+		}
+	}
+}
