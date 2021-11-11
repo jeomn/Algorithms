1f8e8d9bca51af6c6636526b548f9a51d2c87a51
Ji-Min Jeong <46081831+jeomn@users.noreply.github.com>
2021-11-11 19:30:00 +0900

    Create [1008]A(나누기)B.java

---
+++ JAVA_BaekjoonOnlineJudge/입출력과 사칙연산/[1008]A(나누기)B.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Main {
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		double A = Double.parseDouble(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		
+		System.out.println(A/B);
+	}
+}
